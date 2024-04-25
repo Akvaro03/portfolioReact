3113e74fab8d0509b5df22a0defbed208e010daf
Akvaro03 <alvaroballarini2010@gmail.com>
2024-04-25 10:28:31 -0300

    fix subtittle about me and button contact me

--- src/components/aboutMe/index.tsx
+++ src/components/aboutMe/index.tsx
@@ -12,7 +12,7 @@ function AboutMe() {
                 <Avatar src={mePhoto} sx={{ width: "150px", height: "150px" }} alt="Remy Sharp" />
                 <h3>Hi!I'm Alvaro Ballarini {text}</h3>
                 <span>I am a software developer at Consulr Srl, a company dedicated to creating hybrid applications with .NET and web pages based on React. I love programming and innovation, and I am always looking to learn new technologies and tools to improve my projects and solutions.</span>
-                <Button href="mailto:alvaroballarini04@gmail.com" variant="outlined" color="inherit" sx={{ width: "30%", border:"solid 2px", margin:"10px 0" }}>Contact Me</Button>
+                <Button className={Style.buttonContactMe}  href="mailto:alvaroballarini04@gmail.com" variant="outlined" sx={{ width: "30%", border: "solid 2px", margin: "10px 0" }}>Contact Me</Button>
             </article>
             <article className={Style.imgAboutMe}>
                 <img src={aboutMeImage} />
--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -7,20 +7,65 @@ import Sql from "../../assets/img/sql.svg"
 import React from "../../assets/img/react.svg"
 function Skills() {
     return (
-        <article className={Style.containerSkills}>
-            <section className={Style.SkillsComponent}>
-                <h3 className={Style.tittleSkills}>Skills</h3>
-                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias id maiores necessitatibus aliquam quia ipsa, eius harum sapiente magnam ut tempore repudiandae, odit vero nam sit error? Quos, reiciendis veniam.</span>
-                <section className={Style.contentSkills}>
-                    {icons.map((icon, key) => (
-                        <img key={key} className={Style.iconSkills} src={icon} />
-                    ))}
-                </section>
-                <section>
-                    {softSkills.join(" - ")}
-                </section>
-            </section>
-        </article>
+        <div></div>
+        // <article className={Style.containerSkills}>
+        //     <section className={Style.SkillsComponent}>
+        //         <h3 className={Style.tittleSkills}>Skills</h3>
+        //         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias id maiores necessitatibus aliquam quia ipsa, eius harum sapiente magnam ut tempore repudiandae, odit vero nam sit error? Quos, reiciendis veniam.</span>
+        //         <section className={Style.contentSkills}>
+        //             {icons.map((icon, key) => (
+        //                 <img key={key} className={Style.iconSkills} src={icon} />
+        //             ))}
+        //         </section>
+        //         <section>
+        //             {softSkills.join(" - ")}
+        //         </section>
+        //     </section>
+        // </article>
+        // <div className="aix arx cfj">
+        //     <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        //         <h2 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+        //             Trusted by the world’s most innovative teams
+        //         </h2>
+        //         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        //             <img
+        //                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        //                 src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+        //                 alt="Transistor"
+        //                 width={158}
+        //                 height={48}
+        //             />
+        //             <img
+        //                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        //                 src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+        //                 alt="Reform"
+        //                 width={158}
+        //                 height={48}
+        //             />
+        //             <img
+        //                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        //                 src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+        //                 alt="Tuple"
+        //                 width={158}
+        //                 height={48}
+        //             />
+        //             <img
+        //                 className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+        //                 src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+        //                 alt="SavvyCal"
+        //                 width={158}
+        //                 height={48}
+        //             />
+        //             <img
+        //                 className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+        //                 src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+        //                 alt="Statamic"
+        //                 width={158}
+        //                 height={48}
+        //             />
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 
