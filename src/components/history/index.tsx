282496bbac1dd8aa231727cb46d659edb122d17e
Akvaro03 <alvaroballarini2010@gmail.com>
2024-04-26 16:33:02 -0300

    start history section

--- src/components/history/index.tsx
+++ src/components/history/index.tsx
@@ -1,7 +1,21 @@
+import { useRef } from "react";
+import Style from "./history.module.css"
+import { motion } from "framer-motion";
+import { Skeleton } from "@mui/material";
+
 function History() {
+    const containerRef = useRef(null)
     return (
-        <section>
-            <h2>What i did?</h2>
+        <section ref={containerRef} className={Style.containerHistory}>
+            <motion.article
+                className={Style.scrollAndTittle}
+                initial={{ position: "static" }}
+                whileInView={{ position: "sticky", top: "15px" }}
+                viewport={{ root: containerRef }}
+            >
+                <h2>What i did?</h2>
+            </motion.article>
+            <Skeleton height={"110vh"} />
         </section>
     );
 }
